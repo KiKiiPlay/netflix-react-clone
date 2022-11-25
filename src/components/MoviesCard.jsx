a9a2ff52fee279571ebe4fa02cdc1ea1c69f1c04
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-25 15:49:17 +0100

    Creating spinner loading upon MovieCards

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -1,9 +1,11 @@
 import Card from "react-bootstrap/Card";
 import { Component } from "react";
+import Spinner from "react-bootstrap/Spinner";
 
 class MoviesCard extends Component {
   state = {
-    movieArr: {},
+    movieArr: [],
+    isLoading: true,
   };
 
   fetchMovies = async () => {
@@ -14,14 +16,21 @@ class MoviesCard extends Component {
       if (response.ok) {
         let data = await response.json();
         this.setState({
-          movieArr: data,
+          movieArr: data.Search,
+          isLoading: false,
         });
         console.log(this.state.movieArr);
       } else {
         console.log("error fetching Movies");
+        this.setState({
+          isLoading: false,
+        });
       }
     } catch (error) {
       console.log(error);
+      this.setState({
+        isLoading: false,
+      });
     }
   };
 
@@ -31,13 +40,23 @@ class MoviesCard extends Component {
   }
 
   render() {
+    const secondFilm = this.state.movieArr.slice(1, 2);
     return (
       <>
-        {/* {this.state.movieArr.Search.map((element) => (
-          <Card style={{ width: "200px", height: "100px" }} className="mx-auto">
+        {this.state.isLoading && (
+          <Spinner
+            animation="border"
+            role="status"
+            className="custom-spinner-color"
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        )}
+        {secondFilm.map((element) => (
+          <Card style={{ width: "200px", height: "290px" }} className="mx-auto">
             <Card.Img variant="top" src={element.Poster} />
           </Card>
-        ))} */}
+        ))}
       </>
     );
   }
